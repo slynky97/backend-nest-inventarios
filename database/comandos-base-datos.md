edfebfd8feef5409d66068aae2023bbc0ee1f0d3
juan marcelo catari ticona <50919460+slynky97@users.noreply.github.com>
2025-10-20 22:50:21 -0400

    configuracion con bases de datos postgres con typeorm + postgres

---
+++ database/comandos-base-datos.md
@@ -0,0 +1,6 @@
+# Base de datos
+## configurando base de datos
+- Instalar TypreORM con postgres
+```
+npm install @nestjs/typeorm typeorm pg
+```
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '12345',
+      database: 'backend_nest_inventario',
+      entities: [
+        __dirname + '/../**/*.entity{.ts,.js}',
+      ],
+      synchronize: false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
